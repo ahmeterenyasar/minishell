c7d4e9cffbeb7469ce46ee13cc9b3ffc6bf87a32
Ramazan Aktaş <raktas@k1m22s06.42istanbul.com.tr>
2025-07-10 21:44:38 +0300

    signals fixed; leaks exist

--- src/signals/signal_handlers.c
+++ src/signals/signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: raktas <raktas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:55:19 by ayasar            #+#    #+#             */
-/*   Updated: 2025/07/10 20:32:39 by raktas           ###   ########.fr       */
+/*   Updated: 2025/07/10 21:43:51 by raktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 void	handle_sigint_interactive(int signo)
 {
 	(void)signo;
-	g_signal = SIGINT;
 	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
 	rl_replace_line("", 0);
